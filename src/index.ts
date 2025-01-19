ed80b4d7d88d3a491f988fce1d7048a34f478208
OneDongua <875658697@qq.com>
2025-01-19 14:05:44 +0800

    add test

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import express from "express";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+let rootMessage = "Hello, TypeScript and Express!";
 
 app.get("/", (req, res) => {
-  res.send("Hello, TypeScript and Express!");
+  res.send(rootMessage);
+});
+
+app.post("/upload", (req, res) => {
+  console.log(req.body);
+  if (req.body.msg) {
+    rootMessage = req.body.msg;
+    res.status(200).send("Upload success.");
+  } else {
+    res.status(400).send("Bad request: 'msg' parameter is required.");
+  }
 });
 
 app.listen(PORT, () => {
